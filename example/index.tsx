47ae5a5a1c7bc187a7f7c35c8d6cc63465b828f7
Diego Pasquali <imdiegopasquali@gmail.com>
2020-02-22 03:00:04 +0100

    Fixed example dist

--- example/index.tsx
+++ example/index.tsx
@@ -1,10 +1,10 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Kubefont from '../.';
+import 'react-app-polyfill/ie11'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import Kubefont from '../.'
 
 const App = () => {
-  return <Kubefont text="HELLO WORLD" textFontUrl="/font.json" />;
-};
+  return <Kubefont text='HELLO WORLD' textFontUrl='font.json' />
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
